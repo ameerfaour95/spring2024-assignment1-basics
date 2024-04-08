ee2f2f1db6baf36074a60621d69b4bf75f3362c2
Zitong Yang <zitong@sh03-ln05.stanford.edu>
2024-04-07 20:28:09 -0700

    speed test passed

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -1,8 +1,19 @@
+# pytest /home/groups/candes/zitong/cs336-assignment1-basics/tests/test_train_bpe.py
 import regex as re
 from typing import Iterable
 
-from utils import GPT2_PRETOKENIZER_PATTERN
+from cs336_basics.utils import GPT2_PRETOKENIZER_PATTERN
 
+def _contain_pair(byte_tuple: Iterable[bytes], byte_pair: Iterable[bytes]):
+    """
+    Check if the byte pair is in the byte tuple.
+    If so, return a dictionary with the prefix, pair, and suffix.
+    """
+    for i in range(len(byte_tuple) - 1):
+        if byte_tuple[i:i+2] == byte_pair:
+            return dict(prefix=byte_tuple[:i], middle=byte_pair, suffix=byte_tuple[i+2:])
+    return None
+                
 
 def train_bpe(input_path: str, vocab_size: int, special_tokens: Iterable[str]):
     """
@@ -29,43 +40,76 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: Iterable[str]):
     for token in special_tokens:
         text = text.replace(token, "")
     
-    # Perform pre-tokenization and output the frequency table of the tokens
+    # Initialize pre-token frequency table
     pretoken_freq = {}
     for pretoken in re.findall(GPT2_PRETOKENIZER_PATTERN, text):
-        pretoken_encoded = pretoken.encode("utf-8")
-        if pretoken_encoded not in pretoken_freq:
-            pretoken_freq[pretoken_encoded] = 0
-        pretoken_freq[pretoken_encoded] += 1
+        pretoken_tuple = tuple([bytes([b]) for b in tuple(pretoken.encode("utf-8"))])
+        if pretoken_tuple not in pretoken_freq:
+            pretoken_freq[pretoken_tuple] = 0
+        pretoken_freq[pretoken_tuple] += 1
+    
+    # Initialize pair frequency table
+    pair_freq = {}
+    for pretoken_tuple, freq in pretoken_freq.items():
+        for i in range(len(pretoken_tuple) - 1):
+            pair = pretoken_tuple[i:i+2]
+            pair_freq[pair] = pair_freq.get(pair, 0) + freq
         
     # Initialize the merges list
     merges = []
 
     # Perform the BPE algorithm
-    for i in range(vocab_size):
-        # Find the most frequent pair of tokens
-        max_freq = 0
-        max_pair = None
-        for pair in pretoken_freq:
-            if pretoken_freq[pair] > max_freq:
-                max_freq = pretoken_freq[pair]
-                max_pair = pair
-        
-        # Add the pair to the merges list
-        merges.append(max_pair)
-        
-        # Update the frequency table
-        new_pair = max_pair[0] + max_pair[1]
-        pretoken_freq[new_pair] = 0
-        for pretoken in pretoken_freq:
-            pretoken_freq[pretoken] = pretoken_freq[pretoken].replace(max_pair, new_pair)
-    
+    while len(vocab) < vocab_size:
+        # Find the most frequent pair
+        most_freq_pair = max(pair_freq, key=pair_freq.get)
 
+        # Add the pair to the merges list
+        merges.append(most_freq_pair)
+        
+        # Update the vocab
+        new_id = max(vocab.keys()) + 1
+        vocab[new_id] = b"".join(most_freq_pair)
+
+        # Update the pre-token frequency table and pair frequency table
+        new_pretoken_freq = {}
+        for pretoken_tuple, freq in pretoken_freq.items():
+            overlap = _contain_pair(pretoken_tuple, most_freq_pair)
+            if not overlap:
+                new_pretoken_freq[pretoken_tuple] = freq
+            else:
+                # Update the pre-token frequency table
+                new_pretoken = overlap["prefix"] + (vocab[new_id],) + overlap["suffix"]
+                new_pretoken_freq[new_pretoken] = freq
+
+                # Update the pair frequency table
+                if overlap["prefix"]:
+                    add_pair = (overlap["prefix"][-1], vocab[new_id])
+                    pair_freq[add_pair] = freq
+                    del_pair = (overlap["prefix"][-1], most_freq_pair[0])
+                    pair_freq[del_pair] -= freq
+                if overlap["suffix"]:
+                    add_pair = (vocab[new_id], overlap["suffix"][0])
+                    pair_freq[add_pair] = freq
+                    del_pair = (most_freq_pair[1], overlap["suffix"][0])
+                    pair_freq[del_pair] -= freq
+                pair_freq[most_freq_pair] -= freq
+        print(pair_freq[most_freq_pair])
+        pretoken_freq = new_pretoken_freq
+    
+    return vocab, merges
+        
 
     
 
     
 if __name__ == '__main__':
-    tinystory_valid = '/data/TinyStoriesV2-GPT4-valid.txt'
+    # print(_contain_pair((b'ads', b'q', b'bs', b'c', b'd'), (b'bs', b'c'))) 
+    # txt_path = '/home/groups/candes/zitong/cs336-assignment1-basics/tests/fixtures/tinystories_sample_5M.txt'
+    # special_tokens = ['<|endoftext|>']
+    # vocab_size = 100*(10**3)
+    # train_bpe(txt_path, vocab_size, special_tokens)
+
+    txt_path = '/home/groups/candes/zitong/cs336-assignment1-basics/cs336_basics/test_text.txt'
     special_tokens = ['<|endoftext|>']
-    vocab_size = 150*(10**3)
-    train_bpe(tinystory_valid, vocab_size, special_tokens)
+    vocab_size = 260
+    train_bpe(txt_path, vocab_size, special_tokens)
