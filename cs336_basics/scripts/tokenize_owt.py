d7dfd16478e143eee01edaef79f031b4aa484681
Zitong Yang <zitong@sh03-18n13.int>
2024-04-14 19:38:22 -0700

    tokenize owt

---
+++ cs336_basics/scripts/tokenize_owt.py
@@ -0,0 +1,33 @@
+from typing import Iterable
+import numpy as np
+import time
+from tqdm import tqdm
+
+from cs336_basics.tokenizer import Tokenizer
+
+owt = {
+    'train':'data/raw/owt_train.txt',
+    'val':'data/raw/owt_valid.txt',
+    'vocab_filepath': 'data/out/owt_vocab.json',
+    'merges_filepath': 'data/out/owt_merges.txt',
+    'special_tokens': ['<|endoftext|>']
+}
+
+
+tokenizer = Tokenizer.from_files(**owt)
+
+for split in ['train', 'val']:
+    with open(owt[split]) as f:
+        text = f.read()
+    encoded = tokenizer.encode(text, progress_bar=True)
+
+    # save the ids
+    total_batches = 1024
+    batch_size = len(encoded) // total_batches
+    arr = np.memmap(f'data/owt/{split}.bin', dtype=np.uint16, mode='w+', shape=(len(encoded),))
+    idx = 0
+    for batch_idx in tqdm(range(total_batches), desc=f'Writing {split}.bin'):
+        batch = encoded[idx:idx+batch_size]
+        arr[idx:idx+batch_size] = batch
+        idx += batch_size
+arr.flush()
